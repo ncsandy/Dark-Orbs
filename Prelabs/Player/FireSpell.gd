db13bdcea5ff9dff6fe8fae5f3a329ce8fdd17a9
Nick Sandy <69098674+ncsandy@users.noreply.github.com>
2022-08-10 12:54:25 -0400

    Attack fire animation

---
+++ Prelabs/Player/FireSpell.gd
@@ -0,0 +1,10 @@
+extends YSort
+
+onready var fire_down = $FireSpell/FireDown
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func hideFire():
+	firedown.hide()
